5d1d7e9f71b5cbe0c83e3a14fa1f67acc530a0fc
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-03-20 15:29:10 +0700

    xong manytomany

--- src/main/java/com/spring/javaSpring/domain/Order.java
+++ src/main/java/com/spring/javaSpring/domain/Order.java
@@ -1,11 +1,14 @@
 package com.spring.javaSpring.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderDetail> orderDetails;
 
     public long getId() {
         return id;
